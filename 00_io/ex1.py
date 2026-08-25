d8ab8e4aabdaae654e9190c023dc5a0cdf8e7254
Wh1t3 <hyeonjun.yang.20100216@gmail.com>
2026-08-25 15:40:33 +0900

    aaa

--- 00_io/ex1.py
+++ 00_io/ex1.py
@@ -1,2 +1,35 @@
+#입출력 처리
+
+# a = input()
+# print(a)
+# print(type(a))
+
+#정수로 변환
 a = input()
-print(a)
+a = int(a)
+print(a,type(a))
+
+a = int(input())
+print(a,type(a))
+
+#실수입력
+a = float(input())
+print(a,type(a))
+
+# a = int(input())
+# b = int(input())
+# print(a,b)
+
+#100 200
+# a = input().split()
+# print(a,type(a))
+
+#map(함수, 리스트)
+a, b, c = map(int,input().split())
+print(a,type(a))
+print(b,type(b))
+print(c,type(c))
+
+#리스트 변환
+a = list(map(int,input().split()))
+print(a,type(a))
